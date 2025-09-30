ed12e74457bd9288e6fce603fca61101ee0abe5d
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2025-10-01 02:52:30 +0600

    Update pregnancy.js

--- scripts/cmds/pregnancy.js
+++ scripts/cmds/pregnancy.js
@@ -58,10 +58,10 @@ module.exports = {
       ctx.drawImage(template, 0, 0, canvas.width, canvas.height);
 
       // 🎯 অ্যাভাটার বসানো → মেয়েটার মুখ বরাবর
-      const avatarRadius = 220; 
-      const avatarSize = avatarRadius * 3; 
+      const avatarRadius = 222; 
+      const avatarSize = avatarRadius * 2; 
       const avatarX = 260;  // X ঠিক করা হয়েছে
-      const avatarY = 280;  // Y ঠিক করা হয়েছে
+      const avatarY = 285;  // Y ঠিক করা হয়েছে
 
       ctx.save();
       ctx.beginPath();
