8563ffa7d27c613b8fc9b2f60b380f70e8bd81ae
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2025-10-03 11:56:18 +0600

    Update fonts.js

--- scripts/cmds/utils/fonts.js
+++ scripts/cmds/utils/fonts.js
@@ -1,43 +1,37 @@
-const fancySets = {
-  1: "𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔂𝔃" +
-     "𝓐𝓑𝓒𝓓𝓔𝓕𝓖𝓗𝓘𝓙𝓚𝓛𝓜𝓝𝓞𝓟𝓠𝓡𝓢𝓣𝓤𝓥𝓦𝓧𝓨𝓩",
-  2: "𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳" +
-     "𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐐𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙",
-  3: "𝔞𝔟𝔠𝔡𝔢𝔣𝔤𝔥𝔦𝔧𝔨𝔩𝔪𝔫𝔬𝔭𝔮𝔯𝔰𝔱𝔲𝔳𝔴𝔵𝔶𝔷" +
-     "𝔄𝔅ℭ𝔇𝔈𝔉𝔊ℌℑ𝔍𝔎𝔏𝔐𝔑𝔒𝔓𝔔ℜ𝔖𝔗𝔘𝔙𝔚𝔛𝔜ℨ",
-  4: "🅰🅱🅲🅳🅴🅵🅶🅷🅸🅹🅺🅻🅼🅽🅾🅿🆀🆁🆂🆃🆄🆅🆆🆇🆈🆉" +
-     "🅰🅱🅲🅳🅴🅵🅶🅷🅸🅹🅺🅻🅼🅽🅾🅿🆀🆁🆂🆃🆄🆅🆆🆇🆈🆉",
-  5: "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ" +
-     "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ",
-  6: "ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ" +
-     "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  7: "𝑎𝑏𝑐𝑑𝑒𝑓𝑔ℎ𝑖𝑗𝑘𝑙𝑚𝑛𝑜𝑝𝑞𝑟𝑠𝑡𝑢𝑣𝑤𝑥𝑦𝑧" +
-     "𝐴𝐵𝐶𝐷𝐸𝐹𝐺𝐻𝐼𝐽𝐾𝐿𝑀𝑁𝑂𝑃𝑄𝑅𝑆𝑇𝑈𝑉𝑊𝑋𝑌𝑍",
-  8: "ᏴᎾᎢ".toLowerCase() + "ᏴᎾᎢ",
-  9: "𝘢𝘣𝘤𝘥𝘦𝘧𝘨𝘩𝘪𝘫𝘬𝘭𝘮𝘯𝘰𝘱𝘲𝘳𝘴𝘵𝘶𝘷𝘸𝘹𝘺𝘻" +
-     "𝘈𝘉𝘊𝘋𝘌𝘍𝘎𝘏𝘐𝘑𝘒𝘓𝘔𝘕𝘖𝘗𝘘𝘙𝘚𝘛𝘜𝘝𝘞𝘟𝘠𝘡",
-  10:"𝙖𝙗𝙘𝙙𝙚𝙛𝙜𝙝𝙞𝙟𝙠𝙡𝙢𝙣𝙤𝙥𝙦𝙧𝙨𝙩𝙪𝙫𝙬𝙭𝙮𝙯" +
-     "𝘼𝘽𝘾𝘿𝙀𝙁𝙂𝙃𝙄𝙅𝙆𝙇𝙈𝙉𝙊𝙋𝙌𝙍𝙎𝙏𝙐𝙑𝙒𝙓𝙔𝙕",
-  11:"𝙱𝚘𝚝",
-  12:"𝒷ℴ𝓉",
-  13:"𝕭𝖔𝖙",
-  14:"𝖇𝖔𝖙",
-  15:"ＢＯＴ",
-  16:"ʙᴏᴛ",
-  17:"🄱🄾🅃",
-  18:"ᴮᵒᵗ",
-  19:"𝑩𝒐𝒕",
-  20:"𝕭𝕺𝕿"
-};
-
-function toFancy(text, style = 1) {
-  const normal = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const set = fancySets[style] || fancySets[1];
-
-  return text.split("").map(ch => {
-    let i = normal.indexOf(ch);
-    return i !== -1 ? set[i] : ch;
-  }).join("");
-}
-
-module.exports = { toFancy, fancySets };
+module.exports = [
+  text => text,
+  text => text.toUpperCase(),
+  text => text.toLowerCase(),
+  text => `★彡 ${text} 彡★`,
+  text => `❝${text}❞`,
+  text => `『${text}』`,
+  text => `《${text}》`,
+  text => `✦${text}✦`,
+  text => `【${text}】`,
+  text => `〘${text}〙`,
+  text => `☆${text}☆`,
+  text => `♡${text}♡`,
+  text => `『☆${text}☆』`,
+  text => `✪${text}✪`,
+  text => `◉${text}◉`,
+  text => `❂${text}❂`,
+  text => `♛${text}♛`,
+  text => `卐${text}卐`,
+  text => `⟆${text}⟅`,
+  text => `↯${text}↯`,
+  text => `⚡${text}⚡`,
+  text => `➳${text}➳`,
+  text => `✧${text}✧`,
+  text => `⊹${text}⊹`,
+  text => `✿${text}✿`,
+  text => `☽${text}☾`,
+  text => `✵${text}✵`,
+  text => `❀${text}❀`,
+  text => `꧁${text}꧂`,
+  text => `ღ${text}ღ`,
+  text => `✺${text}✺`,
+  text => `༺${text}༻`,
+  text => `✼${text}✼`,
+  text => `☯${text}☯`,
+  text => `✘${text}✘`
+];
